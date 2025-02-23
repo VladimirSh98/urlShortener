72222abe8b95e2800f2bacd1c84711b646ffcac1
Vladimir <V.Shpichko@mail.ru>
2025-02-23 21:58:44 +0300

    add tests for get original url func

--- cmd/shortener/main_test.go
+++ cmd/shortener/main_test.go
@@ -96,6 +96,53 @@ func TestCreateShortURL(t *testing.T) {
 	}
 }
 
+func setupGlobalURLStorageCase() func() {
+	globalURLStorage["TestCase"] = "http://example.com"
+	return func() {
+		delete(globalURLStorage, "TestCase")
+	}
+}
+
 func TestReturnFullURL(t *testing.T) {
-	
+	type expect struct {
+		status   int
+		location string
+	}
+	tests := []struct {
+		description string
+		URL         string
+		expect      expect
+	}{
+		{
+			description: "Test #1. Not exist link",
+			URL:         "/eqwrewerw",
+			expect: expect{
+				status:   http.StatusBadRequest,
+				location: "",
+			},
+		},
+		{
+			description: "Test #2. Success case",
+			URL:         "/TestCase",
+			expect: expect{
+				status:   http.StatusTemporaryRedirect,
+				location: "http://example.com",
+			},
+		},
+	}
+	setup := setupGlobalURLStorageCase()
+	defer setup()
+	for _, test := range tests {
+		t.Run(test.description, func(t *testing.T) {
+			request := httptest.NewRequest(http.MethodGet, test.URL, nil)
+			request.SetPathValue("id", test.URL[1:])
+			w := httptest.NewRecorder()
+			returnFullURL(w, request)
+			result := w.Result()
+			assert.Equal(t, test.expect.status, result.StatusCode)
+			if test.expect.location != "" {
+				assert.Equal(t, test.expect.location, result.Header.Get("Location"))
+			}
+		})
+	}
 }
