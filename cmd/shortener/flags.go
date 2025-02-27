612c3f6db1b0469e0b6b0b848f57f381dd41ccd9
Vladimir <V.Shpichko@mail.ru>
2025-02-27 22:47:30 +0300

    add flags

--- cmd/shortener/flags.go
+++ cmd/shortener/flags.go
@@ -5,7 +5,7 @@ import "flag"
 var flagRunAddr, flagResultAddr string
 
 func parseFlags() {
-	flag.StringVar(&flagRunAddr, "a", "localhost:8888", "Run address")
+	flag.StringVar(&flagRunAddr, "a", "localhost:8000", "Run address")
 	flag.StringVar(&flagResultAddr, "b", "http://localhost:8000", "Result address")
 	flag.Parse()
 }
