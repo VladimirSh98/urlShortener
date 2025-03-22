83e31b7569d496ea5197ce55f0c573868e261043
Vladimir <V.Shpichko@mail.ru>
2025-03-22 12:55:51 +0300

    iter9

--- internal/app/config/schemas.go
+++ internal/app/config/schemas.go
@@ -3,7 +3,7 @@ package config
 type Config struct {
 	ServerAddress string `env:"SERVER_ADDRESS"`
 	BaseURL       string `env:"BASE_URL"`
-	DBFilePath    string `env:"DB_FILE_PATH"`
+	DBFilePath    string `env:"FILE_STORAGE_PATH"`
 }
 
 type defaultConfig struct {
