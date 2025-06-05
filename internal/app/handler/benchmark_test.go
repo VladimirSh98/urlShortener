9e9e09c8a12eb46a55f3faced4cafaaa4e89db07
Vladimir <V.Shpichko@mail.ru>
2025-06-05 22:37:20 +0300

    iter16

---
+++ internal/app/handler/benchmark_test.go
@@ -0,0 +1,25 @@
+package handler
+
+import (
+	"context"
+	"github.com/VladimirSh98/urlShortener/internal/app/database"
+	"github.com/VladimirSh98/urlShortener/internal/app/middleware"
+	dbRepo "github.com/VladimirSh98/urlShortener/internal/app/repository/database"
+	"github.com/VladimirSh98/urlShortener/internal/app/service/shorten"
+	"net/http"
+	"net/http/httptest"
+	"strings"
+	"testing"
+)
+
+func BenchmarkManagerCreateShortURL(b *testing.B) {
+	for i := 0; i < 20; i++ {
+		request := httptest.NewRequest(http.MethodPost, "/", strings.NewReader("http://example.com"))
+		w := httptest.NewRecorder()
+		ctx := context.WithValue(request.Context(), middleware.UserIDKey, 1)
+		repo := dbRepo.ShortenRepository{Conn: database.DBConnection.Conn}
+		service := shorten.NewShortenService(repo)
+		customHandler := NewHandler(service)
+		customHandler.ManagerCreateShortURL(w, request.WithContext(ctx))
+	}
+}
