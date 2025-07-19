1bc1884bd264df4fdc0cc3f8bf52d31d78fe5b13
Vladimir <V.Shpichko@mail.ru>
2025-07-19 14:17:38 +0300

    iter25

--- internal/app/handler/get_stats.go
+++ internal/app/handler/get_stats.go
@@ -3,8 +3,11 @@ package handler
 import (
 	"encoding/json"
 	"github.com/VladimirSh98/urlShortener/internal/app/middleware"
+	myProto "github.com/VladimirSh98/urlShortener/proto"
 	"go.uber.org/zap"
+	"google.golang.org/protobuf/proto"
 	"net/http"
+	"strings"
 )
 
 // GetStats get stats for urls count and users count
@@ -19,16 +22,41 @@ func (h *Handler) GetStats(res http.ResponseWriter, req *http.Request) {
 		URLS:  len(result),
 		Users: int(middleware.UserCount),
 	}
-	jsonResponse, err := json.Marshal(response)
-	if err != nil {
-		sugar.Warnln("GetStats json marshall error", err)
-		res.WriteHeader(http.StatusInternalServerError)
-		return
-	}
-	_, err = res.Write(jsonResponse)
-	if err != nil {
-		sugar.Errorln("GetStats response error", err)
-		res.WriteHeader(http.StatusBadRequest)
-		return
+	accept := req.Header.Get("Accept")
+
+	switch {
+	case strings.Contains(accept, "application/grpc"):
+		grpcResponse := &myProto.StatsResponse{
+			Urls:  int32(response.URLS),
+			Users: int32(response.Users),
+		}
+		var grpcData []byte
+		grpcData, err = proto.Marshal(grpcResponse)
+		if err != nil {
+			sugar.Errorln("Failed to marshal gRPC response:", err)
+			res.WriteHeader(http.StatusInternalServerError)
+			return
+		}
+
+		res.Header().Set("Content-Type", "application/grpc+proto")
+		res.WriteHeader(http.StatusOK)
+		if _, err = res.Write(grpcData); err != nil {
+			sugar.Errorln("Failed to write gRPC response:", err)
+		}
+
+	default:
+		var jsonResponse []byte
+		jsonResponse, err = json.Marshal(response)
+		if err != nil {
+			sugar.Errorln("JSON marshal error:", err)
+			res.WriteHeader(http.StatusInternalServerError)
+			return
+		}
+
+		res.Header().Set("Content-Type", "application/json")
+		res.WriteHeader(http.StatusOK)
+		if _, err = res.Write(jsonResponse); err != nil {
+			sugar.Errorln("Failed to write JSON response:", err)
+		}
 	}
 }
